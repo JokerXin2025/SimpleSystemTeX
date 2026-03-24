bc5544120f36a16a46602b8986dbce235d31093a
JokerXin <zhoukexin@JokerXin.local>
2026-03-24 17:18:30 +0800

    v1.0.0

---
+++ build.lua
@@ -0,0 +1,17 @@
+module = "simplesystem"
+ctanpkg = "simplesystem"
+version = "1.0.0"
+date = "2026-04-01"
+
+sourcefiledir = "src"
+sourcefiles = {
+    "*.code.tex" ,
+    "*.ins"
+}
+
+unpackfiles = {"*.ins"}
+installfiles = {"*.sty"}
+
+textfiles = {"README.md"}
+docfiledir = "./doc/en-US"
+docfiles = {"simplesystem.doc.pdf"}
